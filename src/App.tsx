247cbb8157d875de03e3c0a030299618ff9d708b
Otaiki1 <otaikisadiq2804@gmail.com>
2025-09-24 18:12:33 +0100

    feat: route pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
     return (
-        <>
-            <LandingPage />
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/home" element={<Home />} />
+                {/* <Route path="/game" element={<GameLayout />}>
+                    <Route
+                        index
+                        element={<Game />}
+                        errorElement={<RouteError />}
+                    />
+                    <Route
+                        path="ref/:referralId"
+                        element={<Game />}
+                        errorElement={<RouteError />}
+                    />
+                    <Route path="play" element={<GamePlay />} />
+                    <Route path="leaderboard" element={<Leaderboard />} />
+                    <Route path="friends" element={<Friends />} />
+                    <Route path="classic" element={<ClassicGames />} />
+                </Route> */}
+            </Routes>
+        </Router>
     );
 }
 
