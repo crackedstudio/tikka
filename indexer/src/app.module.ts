298e4c17a905c6f32d9ac9a8ab0859ae9251abac
Enemuo Chimzuruoke <chimzuruoke2017@gmail.com>
2026-02-19 20:49:18 +0100

    feat(indexer): wire cache module and document TTL strategy

--- indexer/src/app.module.ts
+++ indexer/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from './cache/cache.module';
+import { ProcessorsModule } from './processors/processors.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule,
+    ProcessorsModule,
+  ],
   controllers: [],
   providers: [],
 })
