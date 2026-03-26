205df10fe2fc86f07ed24a10dfea9fff04880b86
okekefrancis112 <okeke98@gmail.com>
2026-03-26 11:06:49 +0100

    feat(sdk): add admin operation types

---
+++ sdk/src/modules/admin/admin.types.ts
@@ -0,0 +1,19 @@
+export interface PauseResult {
+  txHash: string;
+  ledger: number;
+}
+
+export interface UnpauseResult {
+  txHash: string;
+  ledger: number;
+}
+
+export interface TransferAdminResult {
+  txHash: string;
+  ledger: number;
+}
+
+export interface AcceptAdminResult {
+  txHash: string;
+  ledger: number;
+}
