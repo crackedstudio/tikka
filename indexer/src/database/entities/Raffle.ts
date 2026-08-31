3122446cee60b37994b7d94e0b38d6b72411151e
Caleb <oluseguncaleb6@gmail.com>
2026-08-31 14:56:16 +0400

    fix(ci): resolve failing checks for #1362

--- indexer/src/database/entities/Raffle.ts
+++ indexer/src/database/entities/Raffle.ts
@@ -1,7 +1,8 @@
 /** @owner indexer */
 import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('raffle')
+
+Entity('raffle')
 export class Raffle {
     @PrimaryGeneratedColumn()
     id: number;
