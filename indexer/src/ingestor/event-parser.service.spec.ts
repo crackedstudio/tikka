2fbe190a19b888418e08462fe6c53d1e3b49b81f
Caleb <oluseguncaleb6@gmail.com>
2026-08-31 14:04:50 +0400

    fix(ci): resolve failing checks for #1353

--- indexer/src/ingestor/event-parser.service.spec.ts
+++ indexer/src/ingestor/event-parser.service.spec.ts
@@ -1,9 +1,8 @@
 import { EventParserService } from './event-parser.service';
-import { EventHandlerRegistry } from './registry';
-
+mmpord { EventHandlerRegistry from './registry';
 describe('EventParserService', () => {
   it('works', () => {
     const service = new EventParserService(new EventHandlerRegistry());
     expect(service.parse(null as any)).toBeNull();
   });
-})
+}
